10767d89ae81530cbab231950bc4253e39388d51
axmed97 <axmed.dev.13@gmail.com>
2023-02-09 14:37:02 +0400

    Js Zero to Hero

---
+++ app.js
@@ -0,0 +1,80 @@
+'use strict'
+
+// let a = 5;
+// // a = a + 5;
+// // a += 5
+// --a
+// console.log(a);
+
+// let a = 2;
+// console.log(++a * a);
+
+// a = 2 * a + a++;
+
+// nici - 6 maqa - 9
+// yu -  8 meri - 8
+// T - 6
+// SAYYAR - 7 Tural - 6
+// let a = 2;
+// a =  4**a;
+// console.log(a);
+
+// let age = +prompt("How old are you?")
+
+// if(age == 21){
+//     console.log('Hemise sen gelesen!')
+//     console.log("Welcome");
+// }else if(age >= 60){
+//     console.log('Get pensiyavi alanda gelersen!')
+// }
+// else if(age > 80){
+//     console.log('Allah omur versin!')
+// }
+// else{
+//     console.log("Good Bye");
+// }
+
+// let a = 21;
+// console.log(a == 21)
+
+// == - qeyri ciddi value yoxlayir
+// === - strong value - data type
+
+// if(5 === 5){
+//     console.log(true);
+// }
+
+// console.log(null <= 0)
+
+// && - and, || - or
+
+// let age = +prompt("Hello");
+
+// console.log(age >= 18 || age <= 40)
+
+// console.log(0 || 6 && 2)
+
+// Loop 
+
+// let limit = 0;
+// const count = 10;
+// while(limit <= count){
+//     const maqa = 'SyaSya';
+//     console.log(limit)
+//     limit+=1;
+// }
+// let i = 0;
+// for(;;)
+// {
+//     console.log(i++)
+// }
+
+// for(let i =0; i<limit; i++){
+
+// }
+let count = 5;
+
+do{
+    count++
+    console.log(count);
+}while(count < 10)
